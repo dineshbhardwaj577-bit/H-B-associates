a98687fe62fcf193503b331f6f4e7c9b6fb71539
dinesh <dinesh.bhardwaj577@gmail.com>
2026-08-03 12:03:50 +0530

    smooth scroll js none

--- assets/js/main.js
+++ assets/js/main.js
@@ -187,18 +187,18 @@ window.addEventListener("load", () => {
 /*==========================
 Smooth Scroll
 ==========================*/
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 
-    anchor.addEventListener("click", function (e) {
+//     anchor.addEventListener("click", function (e) {
 
-        e.preventDefault();
+//         e.preventDefault();
 
-        document.querySelector(this.getAttribute("href")).scrollIntoView({
+//         document.querySelector(this.getAttribute("href")).scrollIntoView({
 
-            behavior: "smooth"
+//             behavior: "smooth"
 
-        });
+//         });
 
-    });
+//     });
 
-});
+// });
